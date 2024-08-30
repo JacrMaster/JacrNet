d0dcf5feab52f0e94b465adfaaf6e85948dec877
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-08-30 11:42:27 -0500

    Creación de la estructura del proyecto

---
+++ src/main/java/com/JacrMaster/JacrNetApplication.java
@@ -0,0 +1,13 @@
+package com.JacrMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacrNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacrNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JacrMaster/JacrNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JacrMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacrNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
