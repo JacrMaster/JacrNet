0f94e308e98de70d2a3e9ddc8020fcad7539c1f0
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-09-20 18:23:43 -0500

    seguridad en el api gateway

---
+++ gateway-service/src/main/java/com/jacrnet/utils/JwtUtils.java
@@ -0,0 +1,39 @@
+package com.jacrnet.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class JwtUtils {
+    @Value("${security.jwt.key.private}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
+    }
+
+
+}
--- user-service/src/main/java/com/jacrnet/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/jacrnet/userservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jacrnet.userservice.config;
 import com.jacrnet.userservice.config.filter.JwtTokenValidator;
 import com.jacrnet.userservice.service.UserDetailServiceImpl;
 import com.jacrnet.userservice.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -19,11 +20,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
     private JwtUtils jwtUtils;
 
     @Bean
